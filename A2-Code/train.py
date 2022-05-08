61c5887c68cfc88aa052757110cdf71fd69d4684
daniel-carrera <dcarrer1@ucsc.edu>
2022-05-07 20:18:56 -0700

    Trigram extraction testing, made function for unigram extraction

--- A2-Code/train.py
+++ A2-Code/train.py
@@ -6,26 +6,49 @@ class NGramLM(object):
         self.n_grams = n_grams
         self.unigram = Counter()
         self.bigram = Counter()
+        self.trigram = Counter()
+        self.stop_token = '<STOP>'
 
+    def trigramExtract(self, data):
+        for sentence in range(0, len(data)):
+            for word in range(0, len(data[sentence]) - 1):
+
+                # if we reached the last word
+                if word == len(data[sentence]) - 2:
+                    word1 = data[sentence][word]
+                    word2 = data[sentence][word+1]
+                    word3 = self.stop_token
+                    self.trigram[(word1, word2, word3)] += 1
+                else:
+                    word1 = data[sentence][word]
+                    word2 = data[sentence][word + 1]
+                    word3 = data[sentence][word + 2]
+                    self.trigram[(word1, word2, word3)] += 1
+
+        print(self.trigram)
 
     def bigramExtract(self, data):
-        start_token = '<START>'
-        stop_token = '<STOP>'
         for sentence in range(0, len(data)):
             for word in range(0, len(data[sentence])):
 
                 # if we reached the last word
                 if word == len(data[sentence]) - 1:
                     word1 = data[sentence][word]
-                    word2 = stop_token
+                    word2 = self.stop_token
                     self.bigram[(word1, word2)] += 1
                 else:
-                    # P(W0, W1) -> tuple (W0, W1)
                     word1 = data[sentence][word]
                     word2 = data[sentence][word+1]
                     self.bigram[(word1, word2)] += 1
-                    
         print(self.bigram)
+
+    def unigramExtract(self, data):
+        for sentence in range(0, len(data)):
+            for word in range(0, len(data[sentence])):
+                self.unigram[data[sentence][word].lower()] += 1
+                if word == len(data[sentence]) - 1:
+                    self.unigram[self.stop_token] += 1
+        print(self.unigram)
 
     def train(self, file_path, needs_preprocess=True):
 
@@ -55,15 +78,14 @@ class NGramLM(object):
         #TRACK COUNTS/PROBABILITIES 
         #use counter dictionary, ngram key, count value
         # how to adjust for ngrams? since key goes from 'Xi' to 'Xi-1 Xi' to 'Xi-2 Xi-1 Xi'
-        for sentence in range(0, len(data)):
-            for word in range(0, len(data[sentence])):
-                self.unigram[data[sentence][word].lower()] += 1
-                if word != len(data[sentence]) - 1:
-                    self.unigram['<STOP>'] += 1
-
-        print(self.unigram)
+        
+        # UNIGRAM EXTRACTION
+        self.unigramExtract(data)
 
         # BIGRAM EXTRACTION
         self.bigramExtract(data)
+
+        # TRIGRAM EXTRACTION
+        self.trigramExtract(data)
 
         return 
