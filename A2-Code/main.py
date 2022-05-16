55f2656eb2502e4accf2ad0d786a400655b0250b
jeremy-lin-11 <92063516+jeremy-lin-11@users.noreply.github.com>
2022-05-15 19:58:55 -0700

    console cleanup

--- A2-Code/main.py
+++ A2-Code/main.py
@@ -21,31 +21,31 @@ def main():
     sample: str = "./data/sample.tokens"
 
 
-    # print("Training uni-gram model")
-    # uni: NGramLM = NGramLM(n_grams=1)
-    # uni.train(train_file, needs_preprocess=True) # will pre-process training file -> preprocessed_train_file 
-    # print("Uni-Gram HDTV PP:", uni.perplexity(testing_file))
-    # print("Uni-Gram train PP:", uni.perplexity(processed_train_file))
-    # print("Uni-gram dev pp:", uni.perplexity(dev_file))
-    # print("Uni-gram test pp:", uni.perplexity(test_file))
+    print("\n   Training uni-gram model")
+    uni: NGramLM = NGramLM(n_grams=1)
+    uni.train(train_file, needs_preprocess=True) # will pre-process training file -> preprocessed_train_file 
+    print("Uni-Gram HDTV PP:", uni.perplexity(testing_file))
+    print("Uni-Gram train PP:", uni.perplexity(processed_train_file))
+    print("Uni-gram dev pp:", uni.perplexity(dev_file))
+    print("Uni-gram test pp:", uni.perplexity(test_file))
 
-    # print("Training bi-gram model")
-    # bi: NGramLM = NGramLM(n_grams=2)
-    # bi.train(processed_train_file, needs_preprocess=False)
-    # print("Bi-Gram HDTV PP:", bi.perplexity(testing_file))
-    # print("Bi-Gram train PP:", bi.perplexity(processed_train_file))
-    # print("Bi-gram dev pp:", bi.perplexity(dev_file))
-    # print("Bi-gram test pp:", bi.perplexity(test_file))
+    print("\n   Training bi-gram model")
+    bi: NGramLM = NGramLM(n_grams=2)
+    bi.train(processed_train_file, needs_preprocess=False)
+    print("Bi-Gram HDTV PP:", bi.perplexity(testing_file))
+    print("Bi-Gram train PP:", bi.perplexity(processed_train_file))
+    print("Bi-gram dev pp:", bi.perplexity(dev_file))
+    print("Bi-gram test pp:", bi.perplexity(test_file))
 
-    # print("Training tri-gram model")
-    # tri: NGramLM = NGramLM(n_grams=3)
-    # tri.train(processed_train_file, needs_preprocess=False)
-    # print("Tri-Gram HDTV PP:", tri.perplexity(testing_file))
-    # print("Tri-Gram train PP:", tri.perplexity(processed_train_file))
-    # print("Tri-gram dev pp:", tri.perplexity(dev_file))
-    # print("Tri-gram test pp:", tri.perplexity(test_file))
+    print("\n   Training tri-gram model")
+    tri: NGramLM = NGramLM(n_grams=3)
+    tri.train(processed_train_file, needs_preprocess=False)
+    print("Tri-Gram HDTV PP:", tri.perplexity(testing_file))
+    print("Tri-Gram train PP:", tri.perplexity(processed_train_file))
+    print("Tri-gram dev pp:", tri.perplexity(dev_file))
+    print("Tri-gram test pp:", tri.perplexity(test_file))
 
-    print("TEST: Linear Interpolation")
+    print("\n   TEST: Linear Interpolation")
     test: NGramLM = NGramLM(n_grams=3)
     test.train(processed_train_file, needs_preprocess=False) # will pre-process training file -> preprocessed_train_file 
     print("Interpolation HDTV PP:", test.interpolation(testing_file))
